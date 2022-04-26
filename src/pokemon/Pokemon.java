7ab9be091a860d0b2974609af523b7b5957d7efb
maarioo <43550926+maarioo@users.noreply.github.com>
2022-04-26 12:50:05 +0200

    get y set nombre

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -2,4 +2,18 @@ package pokemon;
 
 public class Pokemon {
     private String nombre;
+
+    public Pokemon(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
 }
