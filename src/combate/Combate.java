cb3a315435b133181d95f4bb07ad36afc879d8cc
JMCDAM <102923410+JMCDAM@users.noreply.github.com>
2022-05-06 17:48:22 +0200

    Entrenador a falta de 1 metodo y 1 entrenador

---
+++ src/combate/Combate.java
@@ -0,0 +1,58 @@
+package combate;
+
+
+import entrenador.Entrenador;
+
+public class Combate {
+   /*private Entrenador entrenador;
+   private ArrayList<Turno> turno;
+   private int koEntrenador;
+   private int koRival;
+
+    Combate(Entrenador  entrenador, int turno){
+        this.entrenador = entrenador;
+        this.turno = turno;
+    }
+    
+    public Entrenador getEntrenador() {
+        return entrenador;
+    }
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public int getKoEntrenador() {
+        return koEntrenador;
+    }
+
+    public int getKoRival() {
+        return koRival;
+    }
+
+    public void setEntrenador(Entrenador entrenador) {
+        this.entrenador = entrenador;
+    }
+    
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public void setKoEntrenador(int koEntrenador) {
+        this.koEntrenador = koEntrenador;
+    }
+
+    public void setKoRival(int koRival) {
+        this.koRival = koRival;
+    }
+
+
+    public void combate(){
+
+        
+    }
+    
+    public void rendirse(){
+
+    }*/
+}
