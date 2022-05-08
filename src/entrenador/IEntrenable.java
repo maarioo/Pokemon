21995b3e1c32cdbe27980ff82feef560ab6f859b
JMCDAM <102923410+JMCDAM@users.noreply.github.com>
2022-05-08 12:46:23 +0200

    Cambio de pokedex por caja, y metodos a boolean.

--- src/entrenador/IEntrenable.java
+++ src/entrenador/IEntrenable.java
@@ -3,8 +3,8 @@ package entrenador;
 import pokemon.Pokemon;
 
 public interface IEntrenable {
-    public void entrenamientoPesado(Pokemon pokemon);
-    public void entrenamientoFurioso(Pokemon pokemon);
-    public void entrenamientoFuncional(Pokemon pokemon);
-    public void entrenamientoOnirico(Pokemon pokemon);
+    public boolean entrenamientoPesado(Pokemon pokemon);
+    public boolean entrenamientoFurioso(Pokemon pokemon);
+    public boolean entrenamientoFuncional(Pokemon pokemon);
+    public boolean entrenamientoOnirico(Pokemon pokemon);
 }
