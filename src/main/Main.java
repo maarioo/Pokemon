34b6350e3361e505e5a00319add0a140efb1c3f4
JMCDAM <102923410+JMCDAM@users.noreply.github.com>
2022-05-09 21:40:32 +0200

    Guardar main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,16 @@
 package main;
 
-import java.nio.file.WatchEvent;
 import java.util.ArrayList;
-
 import entrenador.Entrenador;
 import pokemon.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
         Pokemon pokemon = new Pokemon();
-        
+
         Entrenador entrenador = new Entrenador("Paco", new ArrayList<Pokemon>(), new ArrayList<Pokemon>());
 
         Entrenador entrenador2 = new Entrenador(4);
 
-
-        
+    }
 }
